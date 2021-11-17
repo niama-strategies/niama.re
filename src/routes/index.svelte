d70af92a49b7d4380b8f9ea4a0543fc28d03dc06
Gregory Bouteiller <gregory.bouteiller@niama.re>
2021-11-17 16:01:45 +0400

    feat: add base seo and favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,14 @@
+<svelte:head>
+  <title>níama stratégies</title>
+  <meta name="description" content={description} />
+  <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+</svelte:head>
 <DomainList />
 
 <script lang="ts">
   import {DomainList} from '$lib/components';
+
+  const description = `níama stratégies vous propose ses services et vous accompagne dans la conception et l'électronique avec níama 
+  conception, le développement applicatif avec níama développement, et enfin l'architecture et l'ingénierie en bâtiment avec níama 
+  ingénierie.`;
 </script>
